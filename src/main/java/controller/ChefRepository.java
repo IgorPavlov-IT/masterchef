9a3f7eb21703e0111953e48a8094c806f12d99c8
IgorPavlov-IT <igor_pavlov@mail.ee>
2020-09-18 11:54:43 +0300

    test

---
+++ src/main/java/controller/ChefRepository.java
@@ -0,0 +1,37 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class ChefRepository {
+
+//    @Autowired
+//    private NamedParameterJdbcTemplate jdbcChefTemplate;
+//
+//    public void createRecipe (recipeDTO newRecipeData) {
+//        String createRecipeString = "INSERT INTO recipe (name, cooking_time, type, notes, instruction)" +
+//                              "VALUES (:name, :cooking_time, :type, :notes, :instruction)";
+//        Map<Object, Object> paramMap = new HashMap<>();
+//        paramMap.put("name" , newRecipeData.getName());
+//        paramMap.put("cooking_time", newRecipeData.getCookingTime());
+//        paramMap.put("type", newRecipeData.getType());
+//        paramMap.put("notes", newRecipeData.getNotes());
+//        paramMap.put("instruction", newRecipeData.getInstruction());
+//        jdbcChefTemplate.update(createRecipeString, paramMap,);
+//
+//
+//
+//    }
+
+    //
+    //
+    //
+    //
+
+
+}
