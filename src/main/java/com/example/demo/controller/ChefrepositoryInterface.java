d9b6749c2037c4723fa1737063868cd173abca04
IgorPavlov-IT <igor_pavlov@mail.ee>
2020-09-22 13:22:43 +0300

    test

---
+++ src/main/java/com/example/demo/controller/ChefrepositoryInterface.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChefrepositoryInterface extends JpaRepository<ChefController.Recipe, Integer> {
+}
+
+
+//categoryRepository.save(category);
+//categoryRepository.getOne(1);
+//categoryRepository.delete(category);
