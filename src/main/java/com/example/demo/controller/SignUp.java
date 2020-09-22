8c4c201bfd62ea18f1d3cb0830c6df583b75289e
Kildu Kask <kildu.kask@gmail.com>
2020-09-22 13:56:28 +0300

    Recipe back töötab!

---
+++ src/main/java/com/example/demo/controller/SignUp.java
@@ -0,0 +1,60 @@
+package com.example.demo.controller;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+//HIBERNATE funktsionaalsuse test// SQL SnakeCase + Java CamelCase
+@Entity
+public class SignUp {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
